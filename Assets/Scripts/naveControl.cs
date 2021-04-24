773322629fc08ca7c3452e21dca1f96fd8aa158c
Waldo Lagos <wlagosdvj@gmail.com>
2021-04-24 16:11:09 -0400

    Herramientas G2

---
+++ Assets/Scripts/naveControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class naveControl : MonoBehaviour
+{
+    private float rotation = 35;
+    public GameObject meteorito;
+    public Slider sliderRot;
+
+    private void Start()
+    {
+        sliderRot.value = rotation;
+    }
+    private void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(sliderRot.value, 70) -35);
+        
+    }
+}
