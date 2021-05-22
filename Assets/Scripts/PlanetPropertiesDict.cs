91f7805d09f55f88b642a335df25bd1f716a7a46
MazeArt <fcoarturo.marshall@gmail.com>
2021-05-22 17:22:07 -0400

    cock/grav/optvalues
    added cockpit, added gravity field and changed opt 1 and opt 2 values

--- Assets/Scripts/PlanetPropertiesDict.cs
+++ Assets/Scripts/PlanetPropertiesDict.cs
@@ -23,35 +23,35 @@ public class PlanetPropertiesDict : MonoBehaviour
     {
         // Build OPT1 Options for all planets ( celestial bodies)
         //planetOptions["optx"] :                       mass,   xAxis, scale,   oribtalPeriod}
-        EarthOptions["opt1"] = new PlanetProperties     (1000f,  151f,  0.12f,     1f);
-        MercuryOptions["opt1"] = new PlanetProperties   (111f,  50f,   1f,           0.5f);
-        VenusOptions["opt1"] = new PlanetProperties     (111f,  107f,  0.08f,  0.8f);
-        MarsOptions["opt1"] = new PlanetProperties      (160f,  231f,  0.06f,  1.88f);
-        JupiterOptions["opt1"] = new PlanetProperties   (5000f, 331f,  0.41f,   11.86f);
-        SaturnOptions["opt1"] = new PlanetProperties    (8000f, 420f,  0.25f,   29.46f);
-        SunOptions["opt1"] = new PlanetProperties       (10f,   0f,    57.35f,    0f);
+        EarthOptions["opt1"] = new PlanetProperties     (1000f, 149f,  0.12f,   1f);
+        MercuryOptions["opt1"] = new PlanetProperties   (111f,  58f,   1f,      0.5f);
+        VenusOptions["opt1"] = new PlanetProperties     (111f,  108f,  0.08f,   0.8f);
+        MarsOptions["opt1"] = new PlanetProperties      (160f,  227f,  0.06f,   1.88f);
+        JupiterOptions["opt1"] = new PlanetProperties   (15000f, 500f,  0.41f,   11.86f);
+        SaturnOptions["opt1"] = new PlanetProperties    (7500f, 720f,  0.25f,   29.46f);
+        SunOptions["opt1"] = new PlanetProperties       (10f,   0f,    90.35f,  0f);
 
         // Build OPT1 Options for all planets ( celestial bodies)
         //Same as opt1 pero sin masa
         //planetOptions["optx"] :                       mass,   xAxis, scale,   oribtalPeriod}
-        EarthOptions["opt0"] = new PlanetProperties(1000f, 151f, 0.12f, 1f);
-        MercuryOptions["opt0"] = new PlanetProperties(0, 50f, 1f, 0.5f);
-        VenusOptions["opt0"] = new PlanetProperties(0, 107f, 0.08f, 0.8f);
-        MarsOptions["opt0"] = new PlanetProperties(0, 231f, 0.06f, 1.88f);
-        JupiterOptions["opt0"] = new PlanetProperties(0, 331f, 0.41f, 11.86f);
-        SaturnOptions["opt0"] = new PlanetProperties(0, 420f, 0.25f, 29.46f);
-        SunOptions["opt0"] = new PlanetProperties(0, 0f, 57.35f, 0f);
+        EarthOptions["opt0"] = new PlanetProperties     (1000f, 151f,  0.12f,   1f);
+        MercuryOptions["opt0"] = new PlanetProperties   (0,     0f,    1f,      0.5f);
+        VenusOptions["opt0"] = new PlanetProperties     (0,     107f,  0.08f,   0.8f);
+        MarsOptions["opt0"] = new PlanetProperties      (0,     231f,  0.06f,   1.88f);
+        JupiterOptions["opt0"] = new PlanetProperties   (0,     331f,  0.41f,   11.86f);
+        SaturnOptions["opt0"] = new PlanetProperties    (0,     420f,  0.25f,   29.46f);
+        SunOptions["opt0"] = new PlanetProperties       (0,     0f,    57.35f,  0f);   
 
 
         // Build OPT2 Options for all ...
         //planetOptions["optx"] :                       mass,   xAxis,  scale,      oribtalPeriod}
-        EarthOptions["opt2"] = new PlanetProperties     (200f,   81f,   0.12f,     1f);
-        MercuryOptions["opt2"] = new PlanetProperties   (111f,   50f,    1f,           0.5f);
-        VenusOptions["opt2"] = new PlanetProperties     (111f,   40f,   0.08f,  0.8f);  
-        MarsOptions["opt2"] = new PlanetProperties      (160f,   121f,   0.06f,  1.88f);   
-        JupiterOptions["opt2"] = new PlanetProperties   (6000f,  208f,   0.25f,   11.86f);
-        SaturnOptions["opt2"] = new PlanetProperties    (7000f,  420f,   0.25f,   29.46f);
-        SunOptions["opt2"] = new PlanetProperties       (10f,    0f,      35.35f,    0f);
+        EarthOptions["opt2"] = new PlanetProperties     (200f,   81f,   0.12f,      1f);
+        MercuryOptions["opt2"] = new PlanetProperties   (111f,   50f,   1f,         0.5f);
+        VenusOptions["opt2"] = new PlanetProperties     (111f,   40f,   0.08f,      0.8f);  
+        MarsOptions["opt2"] = new PlanetProperties      (160f,   121f,  0.06f,      1.88f);   
+        JupiterOptions["opt2"] = new PlanetProperties   (8000f,  208f,  0.25f,      11.86f);
+        SaturnOptions["opt2"] = new PlanetProperties    (5000f,  420f,  0.25f,      29.46f);
+        SunOptions["opt2"] = new PlanetProperties       (10f,    0f,    35.35f,     0f);
 
 
 
